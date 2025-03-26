4d8c8bf4be5213d50d8337c08a9aac10d0dee087
Cikells <07230004.sherubtse@rub.edu.bt>
2025-03-26 21:55:24 +0600

    Practical 3

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const pgp = require('pg-promise')();
 require('dotenv').config();
 const db = pgp({
     host: process.env.DB_HOST || 'localhost', port: 5432, 
-    database: process.env.DB_NAME || 'bookstore_db', 
+    database: process.env.DB_NAME || 'online_bookstore', 
     user: process.env.DB_USER || 'postgres', 
-    password: process.env.DB_PASS || 'yourpassword' 
+    password: process.env.DB_PASS || 'Chekicd@2004' 
 });
 module.exports = db;
